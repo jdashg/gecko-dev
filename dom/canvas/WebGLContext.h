94fdde2b5ec0f0a4010fd4a1eb256d2b3bee54c5
Jeff Gilbert <jdashg@gmail.com>
2015-11-12 15:19:06 -0800

    Fix conformance2/textures/misc/tex-storage-compressed-formats.
    Really the test is wrong, but I updated the tests to check that TEXTURE_3D
    doesn't work for ES3 formats, but that TEXTURE_2D_ARRAY does.

--- dom/canvas/WebGLContext.h
+++ dom/canvas/WebGLContext.h
@@ -1689,8 +1689,9 @@ RoundUpToMultipleOf(const V& value, const M& multiple)
 }
 
 bool
-ValidateTexTarget(WebGLContext* webgl, const char* funcName, GLenum rawTexTarget,
-                  TexTarget* const out_texTarget, WebGLTexture** const out_tex);
+ValidateTexTarget(WebGLContext* webgl, const char* funcName, uint8_t funcDims,
+                  GLenum rawTexTarget, TexTarget* const out_texTarget,
+                  WebGLTexture** const out_tex);
 bool
 ValidateTexImageTarget(WebGLContext* webgl, const char* funcName, uint8_t funcDims,
                        GLenum rawTexImageTarget, TexImageTarget* const out_texImageTarget,
